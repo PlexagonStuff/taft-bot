bbd1df23bafa2f2ab10f739b43affd2e8d6df960
Nicholas-Stokes <plexagongb@gmail.com>
2024-10-29 23:02:54 -0400

    Added texting functionality
    because what could be less annoying then discord message texts?

--- taftbot.py
+++ taftbot.py
@@ -3,11 +3,27 @@ import os
 import discord
 from discord import app_commands
 import random
+import smtplib
+import sys
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+#GUILD = os.getenv('DISCORD_GUILD')
+EMAIL = os.getenv('TEXT_EMAIL')
+PASSWORD = os.getenv('TEXT_PASSWORD')
+PHONENUM = os.getenv('SEND_PHONE')
+PHONECARRIER = os.getenv('SEND_CARRIER')
+
+CARRIER_MAP = {
+    "verizon": "@vtext.com",
+    "tmobile": "@tmomail.net",
+    "sprint": "@messaging.sprintpcs.com",
+    "at&t": "@txt.att.net",
+    "boost": "@smsmyboostmobile.com",
+    "cricket": "@sms.cricketwireless.net",
+    "uscellular": "@email.uscc.net",
+}
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -23,8 +39,21 @@ async def on_ready():
     await tree.sync()
     print(f'We have logged in as {client.user}')
 
+async def sendText(phone_number, carrier, message):
+    recipient = str(phone_number) + CARRIER_MAP[carrier]
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(EMAIL, PASSWORD)
+    senderName = message.author.display_name
+    channel = message.channel.name
+    textMessage = "from " + senderName + " in " + channel + " " + message.content
+    print(textMessage)
+    server.sendmail(EMAIL, recipient, str(textMessage))
+
+
 @client.event
 async def on_message(message):
+    await sendText(PHONENUM,PHONECARRIER, message)
     if message.author == client.user:
         return
     #if message.mentions[0] == client.user:
@@ -35,6 +64,8 @@ async def on_message(message):
         if "How are you feeling" in message.content:
             senderName = message.author.display_name
             await message.channel.send(senderName + ", today I am feeling William Howard Taft!", file=discord.File("taftimages/" + random.choice(image_files)))
+    
+
 @tree.command(
     name="today_im_feeling",
     description="How is Taft Bot feeling today?"
